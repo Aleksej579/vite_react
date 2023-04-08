5adcab57e1253807c0466ccd664c5ed715669ebc
Aleksej579 <aleksej.eremenko579@gmail.com>
2023-04-08 15:59:37 +0300

    о

--- src/components/Xata.jsx
+++ src/components/Xata.jsx
@@ -6,13 +6,14 @@ const page = await xata.db.test.getPaginated();
 
 function Xata() {
   return (
-    <div>
+    <section className='w-3/4 mx-auto'>
+      <h3>Load data from Xata</h3>
       {page.records.map(record => {
         return (
           <p key={record.id}><span>{record.id}</span>: <span>{record.name}</span></p>
         )
       })}
-    </div>
+    </section>
   )
 }
 
